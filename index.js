163dbfc69cf7ad19ccb2e758f0730c519afb3ec6
tejal-supe <supetejalc@gmail.com>
2024-02-22 17:43:02 +0530

    express

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { connect } from "./connection/connect.js";
 
 const app = express();
 const PORT = 8001;
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 connect();
 
 app.listen(PORT,()=>{
